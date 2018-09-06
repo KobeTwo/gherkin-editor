41d434f4bf6adf17162d558aeed8bc31ff522954
Robert Schmidt <r.schmidt@dotsource.de>
2018-09-06 20:10:17 +0200

    initial project

---
+++ src/main/java/de/gherkineditor/gherkineditor/GherkineditorApplication.java
@@ -0,0 +1,12 @@
+package de.gherkineditor.gherkineditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GherkineditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GherkineditorApplication.class, args);
+	}
+}
---
+++ src/test/java/de/gherkineditor/gherkineditor/GherkineditorApplicationTests.java
@@ -0,0 +1,16 @@
+package de.gherkineditor.gherkineditor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GherkineditorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
